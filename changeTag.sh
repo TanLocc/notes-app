ddd387b29cf84619083805ae1b14d12bf025b1b6
Ubuntu <ubuntu@ip-172-31-77-62.ec2.internal>
2022-11-25 16:28:50 +0000

    add code

--- changeTag.sh
+++ changeTag.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd deployk8s
-sed "s/tagVersion/$1/g" app-deployment.yaml > app-deployment.yaml
+sed "s/tagVersion/$1/g" app-deployment.yaml > note-app-deployment.yaml
+rm -rf app-deployment.yaml
